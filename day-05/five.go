d73788ad44ecdbff48e024ab1cfd6778dadc0277
Vladimir Setka <vsetka@gmail.com>
2020-12-06 11:23:33 +0100

    fix: make code less readable with more bitwise operation fun

--- day-05/five.go
+++ day-05/five.go
@@ -7,37 +7,35 @@ import (
 
 /**
 Search example for input -> F B F B B F F
+NOTE: we care about the upper bound only
 
-127		0000000 - 1111111 0 - 127
-64	F 0000000 - 0111111 0 - 63
-32	B 0100000 - 0111111 32 - 63
-16	F 0100000 - 0101111 32 - 47
-8 	B 0101000 - 0101111 40 - 47
-4 	B 0101100 - 0101111 44 - 47
-2 	F 0101100 - 0101101 44 - 45
-1 	F 0101100 44
+127			0000000 - 1111111 0 - 127
+64		F	0000000 - 0111111 0 - 63		clear bit on 0
+32		B 0100000 - 0111111 32 - 63		no clear
+16		F 0100000 - 0101111 32 - 47		clear bit on 2
+8			B 0101000 - 0101111 40 - 47		no clear
+4			B 0101100 - 0101111 44 - 47		no clear
+2			F 0101100 - 0101101 44 - 45		clear bit on 5
+1			F 0101100 44
 */
 
-func bSearch(input string, left rune, right rune) int {
-	upperBound := (1 << len(input)) - 1
-	spread := upperBound
-	lowerBound := 0
+func bSearch(input string, flipper rune) int {
+	// Start with length of input number of 1 bits (1111111 - 127)
+	result := (1 << len(input)) - 1
 
-	for _, splitDirection := range input {
-		spread = spread >> 1
-		if splitDirection == left {
-			upperBound -= spread + 1
-		} else if splitDirection == right {
-			lowerBound += spread + 1
+	for idx, splitDirection := range input {
+		// We only clear on "halving" directions F / L
+		if splitDirection == flipper {
+			result ^= 1 << (len(input) - (idx + 1))
 		}
 	}
 
-	return upperBound
+	return result
 }
 
 func getSeatID(input string) int {
-	row := bSearch(input[:len(input)-3], 'F', 'B')
-	column := bSearch(input[len(input)-3:], 'L', 'R')
+	row := bSearch(input[:len(input)-3], 'F')
+	column := bSearch(input[len(input)-3:], 'L')
 
 	return row*8 + column
 }
